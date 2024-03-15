24cb61fac01316f5088d3b503a184208325d2fb8
newrav1k <kritsky.academi@gmail.com>
2024-03-15 17:34:26 +0300

    lessons java

--- src/university/Homework1/variant3/Therapist.java
+++ src/university/Homework1/variant3/Therapist.java
@@ -1,7 +1,7 @@
 package university.Homework1.variant3;
 
 public class Therapist extends Doctor {
-        @Override
+    @Override
     public String writeRecipe(String recipe) {
         return String.format("Рецепт: Не \"%s\", а спать надо больше!", recipe);
     }
